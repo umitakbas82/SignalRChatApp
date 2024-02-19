d4669f2c6cafb0bbeb61f8ab06ce227ab779b5b8
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-19 12:33:56 +0300

    Json Format issue solved

--- SignalRChatApp/Dtos/UserDto.cs
+++ SignalRChatApp/Dtos/UserDto.cs
@@ -6,7 +6,7 @@ namespace SignalRChatApp.Dtos
     {
         [Required]
         [StringLength(15, MinimumLength =3, ErrorMessage ="Name must be at least {2}, and maximum {1} charecters")]
-        public int Name { get; set; }
+        public string Name { get; set; }
         
     }
 }
